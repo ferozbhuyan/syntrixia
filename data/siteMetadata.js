df51872d3e01675cfcf4cfb10a3e2a42abb83001
ferozbhuyan <feroz.bhuyan@gmail.com>
2026-05-31 12:30:37 -0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: ' Welcome to Syntrixia! ',
   author: 'Feroz Bhuyan',
   headerTitle: ' Website is not ready yet, please visit again!',
-  description: ' Syntrixia is getting ready with worldclass industrial automation solutions'
+  description: ' Syntrixia is getting ready with worldclass industrial automation solutions',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
